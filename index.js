e904f48409bb94b9a96b5f5ea032361f6dfebd7d
陳佳吟 <chen19970221@gmail.com>
2024-06-04 12:05:19 +0800

    修改最終版本

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import "./stylesheets/js/sidebar.js"
 import "./stylesheets/js/data.js"
 import "./stylesheets/js/modal.js"
+
--- stylesheets/js/modal.js
+++ stylesheets/js/modal.js
@@ -3,7 +3,7 @@ const cancelOrderModal = document.querySelector('#cancelOrderModal')
 cancelOrderModal.addEventListener('shown.bs.modal', (e) => {
   const button = e.relatedTarget;
   const orderId = button.dataset.bsOrderId;
-  console.log(button, orderId);
-  const modalText = modalByDelete.querySelector('#deleteText');
+  console.log(button);
+  const modalText = cancelOrderModal.querySelector('#deleteText');
   modalText.textContent = orderId;
 })
